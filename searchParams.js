4e157050f17e3da51a30b77b4d2bd20f058acfe0
MozartKr <hc0517z@gmail.com>
2018-12-18 16:46:01 +0900

    update js files

---
+++ searchParams.js
@@ -0,0 +1,5 @@
+const { URL } = require('url');
+
+const myURL = new URL('http://www.gilbut.co.kr/?page=3&limit=10&category=nodejs&category=javascript');
+console.log('searchParams:', myURL.searchParams);
+console.log('searchParams.getAll():', myURL.searchParams);
